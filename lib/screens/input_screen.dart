bd7d172e87611b9d068a15aa0d3ed64a43c67646
Arenslien <arenslien@gmail.com>
2021-07-12 21:53:18 +0900

    기본 세팅

---
+++ lib/screens/input_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InputScreen extends StatefulWidget {
+  @override
+  _InputScreenState createState() => _InputScreenState();
+}
+
+class _InputScreenState extends State<InputScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
