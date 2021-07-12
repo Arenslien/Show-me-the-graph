8f3ab9942415ad4c9662cc942f9049ce17e107c3
LeeHope <hippo9851@gmail.com>
2021-07-13 02:52:09 +0900

    Stacked Column Graph Demo(graph_all_screen.dart)

--- lib/screens/graph_screen.dart
+++ lib/screens/graph_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class GraphScreen extends StatefulWidget {
   @override
@@ -6,16 +7,16 @@ class GraphScreen extends StatefulWidget {
 }
 
 class _GraphScreenState extends State<GraphScreen> {
+  String g_title = '명사 "엄마"';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Graph Screen")
+        title: Text(g_title),
+        actions: <Widget>[TextButton(onPressed: () {}, child: Text("전체보기"))],
       ),
       body: Center(
-        child: Text(
-          "Graph Screen"
-        ),
+        child: Text("Graph Screen"),
       ),
     );
   }
