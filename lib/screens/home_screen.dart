5533303d3be0eca556efafd8c6d8fa7ba12d347d
Arenslien <arenslien@gmail.com>
2021-07-14 19:41:52 +0900

    Data, Home Update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:show_me_the_graph/screens/no_data_screen.dart';
-import 'package:show_me_the_graph/screens/yes_data_screen.dart';
+import 'package:show_me_the_graph/screens/input_screen.dart';
 import 'package:show_me_the_graph/source/data_source.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,14 +8,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<UnnamedData> temp = <UnnamedData>[];
+  List<GraphData> allData = [];
+
+  // 데이터 베이스에서 모든 데이터를 가져오는 함수
+  List<GraphData> getAllDataFromDB() {
+    List<GraphData> dataList = [];
+    // 가져오는 Logic
+    return dataList;
+  }
+
+  // 상태 초기화 -> 서버에서 모든 데이터를 가져옴
   @override
-  
+  void initState() {
+    super.initState();
+    allData = getAllDataFromDB();
+  }
+
+  // 데이터가 없을 경우 데이터가 없다는 위젯을 생성하는 함수
+  void createNoDataCard() {
+    // 위젯 생성하는 logic
+
+  }
+
+  // 데이터가 있을 경우 각각의 데이터 카드를 생성하는 함수
+  void createAllDataCard() {
+
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    if (temp.length == 0) {
-      return NoDataScreen();
-    } else {
-      return YesDataScreen();
-    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Show Me The Graph"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: allData.length == 0? [] : [],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context, MaterialPageRoute(builder: (context) => InputScreen())
+          );
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
--- lib/source/data_source.dart
+++ lib/source/data_source.dart
@@ -1,12 +1,38 @@
-class UnnamedData {
-  int index = 0;
-  int maxIndex = 0;
-  List<String> name = [];
-  Map<String, List<bool>> data = Map();
+class GraphData {
+  String title;
+  int trueCount = 0;
+  int falseCount = 0;
+  List<bool> allCount = [];
 
-  UnnamedData() {}
+  // GraphData 인스턴스를 만들기 위해서는 제목과 Conut리스트를 인자로 줘야 한다.
+  // ex) GraphData('명사 "엄마"', [0, 0, 1, 1, 1, 1, 1, 0, 1, 1])
+  GraphData(this.title, this.allCount) {
+    for (var value in this.allCount) {
+      if (value == 1) {
+        trueCount++;
+      } else {
+        falseCount++;
+      }
+    }
+  }
 
-  void addData(String newName, List<bool> newData) {
-    name.add(newName);
+  // Title을 return 하는 함수
+  String getTitle() {
+    return title;
+  }
+
+  // trueCount를 return 하는 함수
+  int getTrueCount() {
+    return trueCount;
+  }
+
+  // falseCount를 return 하는 함수
+  int getFalseCount() {
+    return falseCount;
+  }
+
+  // allCount를 return 하는 함수
+  List<bool?> getListData() {
+    return allCount;
   }
 }
